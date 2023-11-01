267611a73388590a21c380314df97445a6b9db6b
= <=>
2023-11-01 20:01:20 +0100

    Default project structure

---
+++ backend/src/config/enviroment.d.ts
@@ -0,0 +1,11 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			CLIENT_ORIGIN: string,
+			NODE_ENV: 'development' | 'production';
+			PORT?: string;
+			MONGODB_URI?:string
+		}
+	}
+}
+export {}
