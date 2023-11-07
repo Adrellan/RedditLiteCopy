1e42db2d0694b2b743078a92722bf01955415a49
Molnár Kristóf Ferenc <molnar.kristof@e-corvina.hu>
2023-11-07 19:53:22 +0100

    Dockerfile created for CI/CD. Lets test this!

--- backend/src/middlewares/performace.mw.ts
+++ backend/src/middlewares/performace.mw.ts
@@ -2,11 +2,11 @@ import {NextFunction, Request, Response} from 'express';
 import chalk from "chalk"
 
 
-export const performanceMiddleware = (req: Request, res: Response, next: NextFunction): void => {
+export const performanceMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 	const startTime = new Date();
 	console.log(chalk.yellow.bold(`${req.method} ${req.url}`))
 	console.log(`🕒  API request started: ${new Date().toLocaleString()}`)
-	next();
+	await next();
 	const endTime = new Date();
 	console.log(`🕒  API request ended: ${new Date().toLocaleString()}`)
 	console.log(`Performance: ${endTime.getTime() - startTime.getTime()}ms\n`)
