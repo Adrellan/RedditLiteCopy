51fba0b8d985bf291e86a0af65d1e1bac59bafec
Orosz Diána <dianaorosz007@gmail.com>
2023-11-09 11:55:44 +0100

    Login és register felület

--- frontend/src/app/page/login/login.component.ts
+++ frontend/src/app/page/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +6,5 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  loginForm=new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required),
-  });
-  login(){
-    if(this.loginForm.invalid) {alert("Érvénytelen felhasználónév vagy jelszó")}
-    else{
-      alert("Még nincs fiókod. Regisztrálj!");
-    }
-  }
+
 }
