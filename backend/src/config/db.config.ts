d01091b78521a24af2258cfa1647b575ec3c2a03
Molnár Kristóf Ferenc <molnar.kristof@e-corvina.hu>
2023-11-10 20:05:21 +0100

    Populates posts with author and comments Populates comments with author and child comments

--- backend/src/config/db.config.ts
+++ backend/src/config/db.config.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv";
 
+
+export const autopopulatePlugin = require("mongoose-autopopulate");
 dotenv.config();
 const mongodbUri: string = process.env.MONGODB_URI || '';
 
