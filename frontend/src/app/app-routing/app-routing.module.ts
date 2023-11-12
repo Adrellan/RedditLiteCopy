bf8a47236fcadf716094015b6d00a848b0c7f305
Fekete Adrian <2001adrian.fekete@gmail.com>
2023-11-12 19:32:36 +0100

    routing ~ ez lehet nem fog kelleni modul után vagy csak részben

--- frontend/src/app/app-routing/app-routing.module.ts
+++ frontend/src/app/app-routing/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../page/home/home.component';
 import { LoginComponent } from '../page/login/login.component';
+import { RegisterComponent } from '../page/register/register.component';
 
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: '**', component: HomeComponent}
 ]
 
