56ed52c5fe96300a55e3786a4d00d179e8db7ea8
Fekete Adrian <2001adrian.fekete@gmail.com>
2023-11-12 19:29:52 +0100

    navbar

--- frontend/src/app/part/navbar/navbar.component.ts
+++ frontend/src/app/part/navbar/navbar.component.ts
@@ -1,137 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+import { LoginComponent } from 'src/app/page/login/login.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
   items: MenuItem[] | undefined;
 
+  constructor(private router: Router, private dialogService: DialogService) {}
+
   ngOnInit() {
-      this.items = [
-          {
-              label: 'File',
-              icon: 'pi pi-fw pi-file',
-              items: [
-                  {
-                      label: 'New',
-                      icon: 'pi pi-fw pi-plus',
-                      items: [
-                          {
-                              label: 'Bookmark',
-                              icon: 'pi pi-fw pi-bookmark'
-                          },
-                          {
-                              label: 'Video',
-                              icon: 'pi pi-fw pi-video'
-                          }
-                      ]
-                  },
-                  {
-                      label: 'Delete',
-                      icon: 'pi pi-fw pi-trash'
-                  },
-                  {
-                      separator: true
-                  },
-                  {
-                      label: 'Export',
-                      icon: 'pi pi-fw pi-external-link'
-                  }
-              ]
-          },
-          {
-              label: 'Edit',
-              icon: 'pi pi-fw pi-pencil',
-              items: [
-                  {
-                      label: 'Left',
-                      icon: 'pi pi-fw pi-align-left'
-                  },
-                  {
-                      label: 'Right',
-                      icon: 'pi pi-fw pi-align-right'
-                  },
-                  {
-                      label: 'Center',
-                      icon: 'pi pi-fw pi-align-center'
-                  },
-                  {
-                      label: 'Justify',
-                      icon: 'pi pi-fw pi-align-justify'
-                  }
-              ]
-          },
-          {
-              label: 'Users',
-              icon: 'pi pi-fw pi-user',
-              items: [
-                  {
-                      label: 'New',
-                      icon: 'pi pi-fw pi-user-plus'
-                  },
-                  {
-                      label: 'Delete',
-                      icon: 'pi pi-fw pi-user-minus'
-                  },
-                  {
-                      label: 'Search',
-                      icon: 'pi pi-fw pi-users',
-                      items: [
-                          {
-                              label: 'Filter',
-                              icon: 'pi pi-fw pi-filter',
-                              items: [
-                                  {
-                                      label: 'Print',
-                                      icon: 'pi pi-fw pi-print'
-                                  }
-                              ]
-                          },
-                          {
-                              icon: 'pi pi-fw pi-bars',
-                              label: 'List'
-                          }
-                      ]
-                  }
-              ]
-          },
-          {
-              label: 'Events',
-              icon: 'pi pi-fw pi-calendar',
-              items: [
-                  {
-                      label: 'Edit',
-                      icon: 'pi pi-fw pi-pencil',
-                      items: [
-                          {
-                              label: 'Save',
-                              icon: 'pi pi-fw pi-calendar-plus'
-                          },
-                          {
-                              label: 'Delete',
-                              icon: 'pi pi-fw pi-calendar-minus'
-                          }
-                      ]
-                  },
-                  {
-                      label: 'Archieve',
-                      icon: 'pi pi-fw pi-calendar-times',
-                      items: [
-                          {
-                              label: 'Remove',
-                              icon: 'pi pi-fw pi-calendar-minus'
-                          }
-                      ]
-                  }
-              ]
-          },
-          {
-              label: 'Quit',
-              icon: 'pi pi-fw pi-power-off'
-          }
-      ];
+    this.items = [
+      {
+        label: 'Bejelentkezés / Regisztráció',
+        icon: 'pi pi-fw pi-sign-out',
+        command: () => {
+          this.showLoginDialog();
+        },
+      },
+      {
+        separator: true,
+      },
+      {
+        label: 'Profil',
+        icon: 'pi pi-fw pi-user',
+      },
+      {
+        label: 'Bejegyzséseim',
+        icon: 'pi pi-fw pi-reddit',
+      },
+    ];
+  }
+
+  showLoginDialog() {
+    const ref = this.dialogService.open(LoginComponent, {
+      header: 'Bejelentkezés',
+      width: '40%',
+      contentStyle: { 'max-height': '50vh', overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: true,
+    });
+
+    ref.onClose.subscribe((response: any) => {});
   }
 }
