aee691998c646963aae703e914892df772478cb4
Fekete Adrian <2001adrian.fekete@gmail.com>
2023-11-12 19:30:13 +0100

    upgraded login

--- frontend/src/app/page/login/login.component.ts
+++ frontend/src/app/page/login/login.component.ts
@@ -1,20 +1,50 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { DialogService } from 'primeng/dynamicdialog';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { RegisterComponent } from '../register/register.component';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  loginForm=new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required),
-  });
-  login(){
-    if(this.loginForm.invalid) {alert("Érvénytelen felhasználónév vagy jelszó")}
-    else{
-      alert("Még nincs fiókod. Regisztrálj!");
-    }
+export class LoginComponent implements OnInit{
+
+  loginData: any = {};
+
+  constructor(
+    private router: Router, 
+    private dialogService: DialogService,
+    private ref: DynamicDialogRef,
+    ) {}
+
+  openRegistrationDialog() {
+    const ref = this.dialogService.open(RegisterComponent, {
+      header: 'Regisztráció',
+      width: '40%',
+      contentStyle: { 'max-height': '50vh', overflow: 'auto' },
+      baseZIndex: 10000,
+      maximizable: true
+    });
+    
+    this.ref.close();
+    // ref.onClose.subscribe((response: any) => {
+    //     if (response) {
+
+    //     }
+    // });
+  }
+
+  ngOnInit(): void {}
+
+  login() {
+    this.ref.close();
+  }
+
+  redirectToHome(){
+    this.router.navigate(['/']);
   }
 }
