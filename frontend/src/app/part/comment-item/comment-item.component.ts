deeab35fdd8d073ddaed519ec9088c40d0e583ed
Molnár Kristóf Ferenc <molnar.kristof@e-corvina.hu>
2023-11-27 23:39:31 +0100

    comment item added!

---
+++ frontend/src/app/part/comment-item/comment-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-item',
+  templateUrl: './comment-item.component.html',
+  styleUrls: ['./comment-item.component.css']
+})
+export class CommentItemComponent implements OnInit{
+  @Input() comment: any
+  ngOnInit(): void {
+  }
+}
